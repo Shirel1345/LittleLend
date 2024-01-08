fb358ef502ac3f1c968c618f8ffd20f81be38629
infraedge <mirir@infraedge.co.il>
2024-01-08 11:49:24 +0200

    fix form

--- src/app/lending-page/lending-page.component.ts
+++ src/app/lending-page/lending-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 
 @Component({
   selector: 'app-lending-page',
@@ -18,24 +22,25 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatFormFieldModule,
     MatButtonModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './lending-page.component.html',
-  styleUrls: ['./lending-page.component.css'],  // השנוי כאן, נכנסת לקובץ CSS ולא styleUrl
+  styleUrls: ['./lending-page.component.css'], // השנוי כאן, נכנסת לקובץ CSS ולא styleUrl
 })
 export class LendingPageComponent {
-  myForm: FormGroup;
+  loanForm: FormGroup;
   constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group({
+    this.loanForm = this.fb.group({
       productName: ['', Validators.required],
-      date: ['', Validators.required],
-      returnDate: ['', Validators.required],
+      loanDate: ['', Validators.required],
+      returnLoanDate: ['', Validators.required],
       depositOptions: ['', Validators.required],
       securityDeposit: ['', [Validators.required, Validators.min(0)]],
     });
 
     // You can add custom validators for date fields here
-    this.myForm.get('date')?.setValidators(this.dateValidator);
-    this.myForm.get('returnDate')?.setValidators(this.dateValidator);
+    this.loanForm.get('loanDate')?.setValidators(this.dateValidator);
+    this.loanForm.get('returnLoanDate')?.setValidators(this.dateValidator);
   }
 
   // Custom date validator function
@@ -45,7 +50,7 @@ export class LendingPageComponent {
 
     // Check if the selected date is before the current date
     if (selectedDate < currentDate) {
-      return { 'invalidDate': true };
+      return { invalidDate: true };
     }
 
     return null;
